b88b2675beca377eaf891f35b0359097f2f29fbb
Jack Ward <stryker2k2@msn.com>
2019-10-23 14:57:03 -0500

    For Loop for Debug Test

--- main.cpp
+++ main.cpp
@@ -1,1 +1,17 @@
-"printf("HelloWorld");" 
+#include <iostream>
+
+int main ()
+{
+    int i;
+    
+    for (i = 0; i < 10; i++)
+    {
+        std::cout << "[-] Value is: " << i+1 << std::endl;
+    }
+
+    std::cout << "[!] Version 0.0.1" << std::endl;
+    std::cout << "[!] Hello World Complete!" << std::endl;
+
+    //std::cin.get();
+    return 0;
+}
